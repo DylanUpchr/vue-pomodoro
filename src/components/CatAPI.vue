fbe705520a8304d0a831796f4e9bbb3285c0a4da
Dylan Upchurch <dylan.upchr@eduge.ch>
2020-01-15 09:04:29 +0100

    Added axios and call to TheCatAPI along with img tag to show returned image

--- src/components/CatAPI.vue
+++ src/components/CatAPI.vue
@@ -1,11 +1,36 @@
 <template>
-  <div></div>
+  <div>
+      <img :src="this.Img.url">
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+    methods: {
+        'fetchImg': function(){
+            var apiUrl = "https://api.thecatapi.com/v1/images/search"
+            //var result
+            return axios.get(apiUrl).then(function(response){
+                //this.Img = response.data[0]
+                //alert(JSON.stringify(response.data[0]))
+                return response.data[0]
+            })
+        }
+    },
+    data: function(){
+        return{
+            Img: null
+        }
+    },
+    mounted(){
+        this.fetchImg().then((response) => {
+            this.Img = response
+        })
+    }
 }
+
+
 </script>
 
 <style>
