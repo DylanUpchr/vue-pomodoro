6066fe6ccbbfd38d88f01ae7d92c4bef25e1f912
Shiirokuma <flocon.25@outlook.com>
2020-02-05 10:44:28 +0100

    Transform tomato to grey with css

--- src/components/PomodoroTomato.vue
+++ src/components/PomodoroTomato.vue
@@ -11,8 +11,27 @@ export default {
 </script>
 
 <style>
+
 .tomato{
   width: auto;
   height: 45vh;
+ /* animation: grayInLoad 2s infinite;*/
+ /* -webkit-animation-direction: alternate-reverse;*/
 }
+/*@-webkit-keyframes grayInLoad {
+  from {
+    filter: grayscale(0%);
+  }
+  to {
+    filter: grayscale(100%);
+  }
+}
+@keyframes grayInLoad {
+  from {
+    filter: grayscale(0%);
+  }
+  to {
+    filter: grayscale(100%);
+  }
+}*/
 </style>
