0224baeff95723bd25d4d207260ad70e4942f620
Shiirokuma <flocon.25@outlook.com>
2020-02-05 11:37:21 +0100

    Transform tomato to grey with css

--- src/components/PomodoroTomato.vue
+++ src/components/PomodoroTomato.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img id="img" class="tomato" alt="Tomato" src="../assets/tomato.jpg">
+    <img hidden id="img" class="tomato" alt="Tomato" src="../assets/tomato.jpg">
     <canvas id="canvas" width="400" height="400"></canvas>
   </div>
 </template>
@@ -13,28 +13,28 @@ export default {
 
       let imgElement = document.getElementById('img')
       imgElement.onload = function(){
-      let src = cv.imread('img')
-      let dst = new cv.Mat()
-      let dsize = new cv.Size(src.rows, src.cols);
-      let center = new cv.Point(src.cols / 2, src.rows / 2);
-      let colorSpaces = [
-        cv.COLOR_RGB2BGRA,
-        cv.COLOR_RGBA2GRAY,
-        cv.COLOR_RGBA2BGR565
-      ]
+        let src = cv.imread('img')
+        let dst = new cv.Mat()
+        let dsize = new cv.Size(src.rows, src.cols);
+        let center = new cv.Point(src.cols / 2, src.rows / 2);
+        let colorSpaces = [
+          cv.COLOR_RGB2BGRA,
+          cv.COLOR_RGBA2GRAY,
+          cv.COLOR_RGBA2BGR565
+        ]
 
         cv.cvtColor(src, dst, colorSpaces[Math.floor(Math.random() * 3)])
         let M = cv.getRotationMatrix2D(center, Math.floor(Math.random() * 180), 1);
         cv.warpAffine(dst, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
         cv.imshow('canvas', dst)
 
-      setInterval(function(){
-        cv.cvtColor(src, dst, colorSpaces[Math.floor(Math.random() * 3)])
-        let M = cv.getRotationMatrix2D(center, Math.floor(Math.random() * 180), 1);
-        cv.warpAffine(dst, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
-        cv.imshow('canvas', dst)
-      },
-      30000)
+        setInterval(function(){
+          cv.cvtColor(src, dst, colorSpaces[Math.floor(Math.random() * 3)])
+          let M = cv.getRotationMatrix2D(center, Math.floor(Math.random() * 180), 1);
+          cv.warpAffine(dst, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+          cv.imshow('canvas', dst)
+        },
+        30000)
       }
     }
 }
